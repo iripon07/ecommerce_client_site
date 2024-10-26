c4b77614effbab5a39a660f08797feda3dcf9dd8
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-26 21:54:26 +0600

    Responsived

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       {/* <Feature /> */}
-      {/* <Offer /> */}
+      <Offer />
       {/* <About /> */}
       <Testimonial />
       <News />
--- src/components/Home/Offer.tsx
+++ src/components/Home/Offer.tsx
@@ -7,22 +7,22 @@ const Offer = () => {
             backgroundImage:
               "linear-gradient(100deg, #051922 0%, #00000000 100%)",
           }}
-          className="grid h-[60vh] w-full grid-cols-2 bg-transparent"
+          className="grid 2xl:h-[55vh] h-[40vh] md:h-[30vh] xl:h-[50vh] w-full xl:grid-cols-2 grid-cols-1 bg-transparent"
         >
-          <div  className="flex items-center px-12">
+          <div  className="flex items-center 2xl:px-12  xl:px-10 lg:px-8 md:px-6 px-4">
             <div  className="">
-              <h5 className="mb-6 text-xl font-bold text-white">
+              <h5 className="lg:mb-6 md:mb-4 mb-2 text-xl font-bold text-white">
                 Limited Time Offer
               </h5>
-              <h3 className="mb-4 text-[42px] font-bold text-secondary">
+              <h3 className="md:mb-4 mb-2 text-[42px] font-bold text-secondary">
                 Special Edition
               </h3>
-              <p className="mb-6 text-sm font-normal text-white">
+              <p className="lg:mb-6 md:mb-4 mb-2 text-sm font-normal text-white">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Cupiditate nisi ut culpa in ullam tempora. Cupiditate nisi ut
                 culpa in ullam tempora.
               </p>
-              <h5 className="mb-10 text-xl font-bold text-white">
+              <h5 className="2xl:mb-10 xl:mb-8 lg:mb-6 md:mb-4 mb-3 text-xl font-bold text-white">
                 Buy gadgets At 20% Discount, Use Code
                 <span className="text-secondary">OFF20</span>
               </h5>
@@ -31,7 +31,7 @@ const Offer = () => {
               </button>
             </div>
           </div>
-          <div></div>
+          <div className="xl:block hidden"></div>
         </div>
       </div>
     </div>
