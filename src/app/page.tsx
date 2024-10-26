c4ecfa7efe4adb28a0cfaf8ea5879a2395695ba8
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-26 22:22:27 +0600

    Fitured responsivedd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import About from "@/components/Home/About";
 import Feature from "@/components/Home/Feature";
 import News from "@/components/Home/News";
 import Offer from "@/components/Home/Offer";
