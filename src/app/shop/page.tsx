420b748dc9f8fd8d7f8ed75297a89f77b37fb965
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-11-26 16:23:25 +0600

    shopcard created

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,9 +1,121 @@
-import React from 'react'
+import iphone16 from "@/assets/iphone-16-pro-max-256gb-black-titanium.jpeg";
+import Image from "next/image";
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <div className="container mx-auto my-10 px-5 font-poppins">
+      <div className="grid grid-cols-5 gap-6">
 
-export default Shop
+        <div className="rounded-md px-3 py-2 duration-300 hover:shadow-md">
+          <Image src={iphone16} alt=" Mobile" />
+          <p className="mt-2 text-center text-2xl font-semibold text-primary">
+            iPhone 16 Pro Max 256GB
+          </p>
+          <p className="my-1 text-center text-lg font-medium text-secondary">
+            ৳ 249000
+          </p>
+          <p className="text-center text-base font-light text-grey">
+            Not Review Yet
+          </p>
+          <div className="mt-2 flex items-center justify-between">
+            <button className="min-w-[120px] rounded-full border-[1px] border-secondary py-2 font-semibold text-secondary duration-300 hover:bg-secondary hover:text-white">
+              Buy Now
+            </button>
+            <button className="min-w-[120px] rounded-full border-[1px] border-primary py-2 font-semibold text-primary duration-300 hover:bg-primary hover:text-white">
+              Add to Card
+            </button>
+          </div>
+        </div>
+
+        <div className="rounded-md px-3 py-2 duration-300 hover:shadow-md">
+          <Image src={iphone16} alt=" Mobile" />
+          <p className="mt-2 text-center text-2xl font-semibold text-primary">
+            iPhone 16 Pro Max 256GB
+          </p>
+          <p className="my-1 text-center text-lg font-medium text-secondary">
+            ৳ 249000
+          </p>
+          <p className="text-center text-base font-light text-grey">
+            Not Review Yet
+          </p>
+          <div className="mt-2 flex items-center justify-between">
+            <button className="min-w-[120px] rounded-full border-[1px] border-secondary py-2 font-semibold text-secondary duration-300 hover:bg-secondary hover:text-white">
+              Buy Now
+            </button>
+            <button className="min-w-[120px] rounded-full border-[1px] border-primary py-2 font-semibold text-primary duration-300 hover:bg-primary hover:text-white">
+              Add to Card
+            </button>
+          </div>
+        </div>
+
+        <div className="rounded-md px-3 py-2 duration-300 hover:shadow-md">
+          <Image src={iphone16} alt=" Mobile" />
+          <p className="mt-2 text-center text-2xl font-semibold text-primary">
+            iPhone 16 Pro Max 256GB
+          </p>
+          <p className="my-1 text-center text-lg font-medium text-secondary">
+            ৳ 249000
+          </p>
+          <p className="text-center text-base font-light text-grey">
+            Not Review Yet
+          </p>
+          <div className="mt-2 flex items-center justify-between">
+            <button className="min-w-[120px] rounded-full border-[1px] border-secondary py-2 font-semibold text-secondary duration-300 hover:bg-secondary hover:text-white">
+              Buy Now
+            </button>
+            <button className="min-w-[120px] rounded-full border-[1px] border-primary py-2 font-semibold text-primary duration-300 hover:bg-primary hover:text-white">
+              Add to Card
+            </button>
+          </div>
+        </div>
+
+        <div className="rounded-md px-3 py-2 duration-300 hover:shadow-md">
+          <Image src={iphone16} alt=" Mobile" />
+          <p className="mt-2 text-center text-2xl font-semibold text-primary">
+            iPhone 16 Pro Max 256GB
+          </p>
+          <p className="my-1 text-center text-lg font-medium text-secondary">
+            ৳ 249000
+          </p>
+          <p className="text-center text-base font-light text-grey">
+            Not Review Yet
+          </p>
+          <div className="mt-2 flex items-center justify-between">
+            <button className="min-w-[120px] rounded-full border-[1px] border-secondary py-2 font-semibold text-secondary duration-300 hover:bg-secondary hover:text-white">
+              Buy Now
+            </button>
+            <button className="min-w-[120px] rounded-full border-[1px] border-primary py-2 font-semibold text-primary duration-300 hover:bg-primary hover:text-white">
+              Add to Card
+            </button>
+          </div>
+        </div>
+
+        <div className="rounded-md px-3 py-2 duration-300 hover:shadow-md">
+          <Image src={iphone16} alt=" Mobile" />
+          <p className="mt-2 text-center text-2xl font-semibold text-primary">
+            iPhone 16 Pro Max 256GB
+          </p>
+          <p className="my-1 text-center text-lg font-medium text-secondary">
+            ৳ 249000
+          </p>
+          <p className="text-center text-base font-light text-grey">
+            Not Review Yet
+          </p>
+          <div className="mt-2 flex items-center justify-between">
+            <button className="min-w-[120px] rounded-full border-[1px] border-secondary py-2 font-semibold text-secondary duration-300 hover:bg-secondary hover:text-white">
+              Buy Now
+            </button>
+            <button className="min-w-[120px] rounded-full border-[1px] border-primary py-2 font-semibold text-primary duration-300 hover:bg-primary hover:text-white">
+              Add to Card
+            </button>
+          </div>
+        </div>
+
+       
+      
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
